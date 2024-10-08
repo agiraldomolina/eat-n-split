2450c42920b571e19831723e42f9cc76b28d5247
Alba Giraldo <agiraldomolina@gmail.com>
2024-10-08 12:19:28 -0400

    Building the static App: List of friends

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import FriendList from "./FriendsList";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+export default function App(){
+  const [friends, setFriends] = useState(initialFriends);
+  return(
+    <div className="app">
+      <div className="sidebar">
+        <FriendList friends={friends}/>
+      </div>
+    </div>
+  )
+
+}
