22684c6d4dcf4b1869662b3faab1c3506e4cf902
Alba Giraldo <agiraldomolina@gmail.com>
2024-10-08 21:30:23 -0400

    Creating ontrolled Elements

--- src/components/FormSplitBill.js
+++ src/components/FormSplitBill.js
@@ -1,20 +1,37 @@
+import { useState } from "react";
 import Button from "./Button";
 export default function FormSplitBill({selectedFriend}){
-    // console.log('from FormSplitBill: ', selectedFriend)
+    const [bill, setBill] = useState("");
+    const [paidByUser, setPaidByUser] = useState("");
+    const [whoIsPaying, setWhoIsPaying] = useState("user");
+
+    const paidByFriend =bill ? bill - paidByUser : "";
+        
 
     return <form className="form-split-bill">
         <h2>Split a bill with {selectedFriend.name}</h2>
         <label>💰 Bill value</label>
-        <input type="number"></input>
+        <input type="text" onChange={(e)=>setBill(Number(e.target.value))}/>
 
         <label>💁 Your expense</label>
-        <input type="number"></input>
+        <input 
+            type="text" 
+            onChange={(e)=>
+                setPaidByUser(
+                    Number(e.target.value) > bill ? paidByUser:
+                    Number(e.target.value)
+                )
+            }
+        />
 
-        <label>🧑‍🤝‍🧑 X's expense </label>
-        <input type="number" disabled></input>
+        <label>🧑‍🤝‍🧑 {selectedFriend.name}'s expense </label>
+        <input type="number" disabled value={paidByFriend}/>
 
         <label>🤑 Who is paying the bill</label>
-        <select>
+        <select
+            value={whoIsPaying}
+            onChange={(e)=>setWhoIsPaying(e.target.value)}
+        >
             <option value='user'>You</option>
             <option value='friend'>{selectedFriend.name}'s</option>
         </select>
